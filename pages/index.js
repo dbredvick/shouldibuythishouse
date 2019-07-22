8a9b091c22fac8a90dec509b30b2fccd511b9ea4
Drew Bredvick <drew.bredvick@spreetail.com>
2019-07-21 20:51:27 -0500

    working material-ui

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
-import Header from "../components/header";
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import MuiLink from '@material-ui/core/Link';
+import ProTip from '../src/ProTip';
+import Link from '../src/Link';
 
-function Index() {
+export default function Index() {
   return (
-    <main>
-      <Header />
-      <section>
-
-      </section>
-    </main>
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Should I Buy This House
+        </Typography>
+      </Box>
+    </Container>
   );
 }
-
-export default Index;
