bb3fe7041c2d62b9127a996d95976e1a49150735
Drew Bredvick <drew.bredvick@spreetail.com>
2019-07-28 20:19:23 -0500

    working secret configuration

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,63 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import MuiLink from '@material-ui/core/Link';
-import TextField from '@material-ui/core/TextField';
-
+import {
+  Box,
+  Container,
+  Divider,
+  TextField,
+  Typography,
+} from '@material-ui/core';
 export default function Index() {
   return (
     <Container maxWidth="sm">
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Should I Buy This House
+          Should I Buy This House?
         </Typography>
       </Box>
       <TextField
-        id="outlined-zillow"
-        label="Zillow Link"
+        id="outlined-link"
+        label="Link"
         margin="normal"
         variant="outlined"
-        helperText="Copy and paste your link from Zillow and press Enter"
+        helperText="Paste a link to the house listing if you have one."
+        fullWidth
+      />
+      <TextField
+        id="outlined-city"
+        label="City"
+        margin="normal"
+        variant="outlined"
+        fullWidth
+      />
+      <TextField
+        id="outlined-state"
+        label="State"
+        margin="normal"
+        variant="outlined"
+        fullWidth
+      />
+      <TextField
+        id="outlined-cost"
+        label="Home cost"
+        margin="normal"
+        variant="outlined"
+        helperText="What's the total cost of your home?"
+        fullWidth
+      />
+      <TextField
+        id="outlined-downpayment"
+        label="Down payment"
+        margin="normal"
+        variant="outlined"
+        helperText="Amount you are putting down. This does not include closing costs."
+        fullWidth
+      />
+      <TextField
+        id="outlined-taxes"
+        label="Taxes"
+        margin="normal"
+        variant="outlined"
+        helperText="Enter your best guess at local property taxes."
         fullWidth
       />
     </Container>
